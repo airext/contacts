adfd2a19d7a5ef6edd6d987a8f2d8b66008b4bcb
Max Rozdobudko <max.rozdobudko@gmail.com>
2013-09-11 09:49:11 +0300

    Adds ContactsDebug project for debugging ANE code.

---
+++ contacts-ios/Contacts/ContactsDebug/ContactsDebugAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ContactsDebugAppDelegate.h
+//  ContactsDebug
+//
+//  Created by Maxim on 9/10/13.
+//  Copyright (c) 2013 Max Rozdobudko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContactsDebugAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ contacts-ios/Contacts/ContactsDebug/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ContactsDebug
+//
+//  Created by Maxim on 9/10/13.
+//  Copyright (c) 2013 Max Rozdobudko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ContactsDebugAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ContactsDebugAppDelegate class]));
+    }
+}
